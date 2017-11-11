1121fef08652bcc20687c5b4b808fdd673be8462
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2017-11-11 21:07:12 +0700

    Add model example on Product

---
+++ src/schema/models/Product/index.ts
@@ -0,0 +1,64 @@
+import * as Iridium from 'iridium';
+import { Collection, Index, Instance, Model, ObjectID, Property } from 'iridium';
+
+interface IProductDocument {
+    _id?: string;
+    name: string;
+    description?: string;
+
+    price: number;
+
+    picture?: string[];
+    hashtag?: string[];
+    colors?: string[];
+    sizes?: string[];
+
+    promotion_start: Date;
+    createAt?: Date;
+    updateAt?: Date;
+}
+
+@Index({
+    name: 1,
+    hastag: 1,
+    promotion_start: 0,
+})
+@Collection('products')
+class Product extends Instance<IProductDocument, Product> implements IProductDocument {
+  @ObjectID
+  _id: string;
+  @Property(/^.+$/, true)
+  name: string;
+  @Property(String, false)
+  description: string;
+
+  @Property(Number, true)
+  price: number;
+
+  @Property([String], false)
+  picture: string[];
+  @Property([String], false)
+  hashtag: string[];
+  @Property([String], false)
+  colors: string[];
+  @Property([String], false)
+  sizes: string[];
+
+  @Property(Date, true)
+  promotion_start: Date;
+  @Property(Date, false)
+  createAt: Date;
+  @Property(Date, false)
+  updateAt: Date;
+
+  static onCreating(product: IProductDocument) {
+    product.createAt = new Date();
+    product.updateAt = new Date();
+  }
+
+  static onSaving(product: Product, changes: Iridium.Changes) {
+    product.updateAt = new Date();
+  }
+}
+
+export { IProductDocument, Product };
--- src/schema/models/index.ts
+++ src/schema/models/index.ts
@@ -1,9 +1,11 @@
 import { Core, Model } from 'iridium';
 import { mongodb } from '../../config';
+import { IProductDocument, Product } from './Product';
 import { IUserDocument, User } from './User';
 
 class Database extends Core {
   User = new Model<IUserDocument, User>(this, User);
+  Product = new Model<IProductDocument, Product>(this, Product);
 }
 
 const database = new Database({ ...mongodb });
