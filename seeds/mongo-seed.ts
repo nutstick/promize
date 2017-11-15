b3b874ec94dfaf70512267e513702a6106c19413
player <ammarinjtk@outlook.com>
2017-11-15 16:57:52 +0700

    Edit seeds to generate multiple items in mongoldb

--- seeds/mongo-seed.ts
+++ seeds/mongo-seed.ts
@@ -15,32 +15,37 @@ export async function seed(database: Database) {
   // Clear database
   await database.connection.dropDatabase();
   // Start seeding
-  const user = await database.User.create({
-    first_name: m.first_name,
-    last_name: m.last_name,
-    tel_number: m.phone,
-    account: {
-      email: m.email,
-      password: m.password,
-    },
-    avatar: m.url,
-  });
+  for (let i = 0; i <= 20; i++) {
+    const user = await database.User.create({
+      first_name: m.first_name,
+      last_name: m.last_name,
+      tel_number: m.phone,
+      account: {
+        email: m.email,
+        password: m.password,
+      },
+      avatar: m.url,
+    });
 
-  // mm.ra
-  await database.Product.create({
-    name: m.word,
-    description: m.description,
+    // mm.ra
+    for (let j = 0; j <= Math.floor(Math.random() * 4); j++) {
+      await database.Product.create({
+        name: m.word,
+        description: m.description,
 
-    price: m.integer(100, 2000),
+        price: m.integer(100, 2000),
 
-    picture: ['https://th-live-02.slatic.net/p/7/hequ-1483111676-123106' +
-      '5-c566b543a82cfe5a0e279dbf161bd13e-catalog_233.jpg'],
-    hashtag: array_of(Math.floor(Math.random() * 3), () => m.word),
-    colors: array_of(Math.floor(Math.random() * 3), () => m.color_name),
-    sizes: array_of(Math.floor(Math.random() * 3), () => m.random_element(['S', 'M', 'L'])),
+        picture: ['https://th-live-02.slatic.net/p/7/hequ-1483111676-123106' +
+          '5-c566b543a82cfe5a0e279dbf161bd13e-catalog_233.jpg'],
+        hashtag: array_of(Math.floor(Math.random() * 3), () => m.word),
+        colors: array_of(Math.floor(Math.random() * 3), () => m.color_name),
+        sizes: array_of(Math.floor(Math.random() * 3), () => m.random_element(['S', 'M', 'L'])),
 
-    promotion_start: start_date.toDate(),
-    promotion_end: end_date.toDate(),
-    owner: user._id,
-  });
+        promotion_start: start_date.toDate(),
+        promotion_end: end_date.toDate(),
+        owner: user._id,
+      });
+    }
+  }
+
 }
