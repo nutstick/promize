58a4ea77d6cab88768c05266c42c950ff0b90927
Pathanin <plokbow@gmail.com>
2017-11-17 12:34:47 +0700

    add facebook key

--- src/config.ts
+++ src/config.ts
@@ -54,9 +54,9 @@ export const auth = {
 
   // https://developers.facebook.com/
   facebook: {
-    id: process.env.FACEBOOK_APP_ID || '186244551745631',
+    id: process.env.FACEBOOK_APP_ID || '1946061138994426',
     secret:
-      process.env.FACEBOOK_APP_SECRET || 'a970ae3240ab4b9b8aae0f9f0661c6fc',
+      process.env.FACEBOOK_APP_SECRET || 'f662d8cdf0bc78f455db8f45eeda6a81',
   },
 
   // https://cloud.google.com/console/project
