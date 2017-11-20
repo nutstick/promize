6c25bca9bc98d3dc0c4b745571b20d4fbe53f899
ChanatipSaetia <chanatippung@gmail.com>
2017-11-21 01:59:36 +0700

    order receipt in mongo and graphql

--- seeds/mongo-seed.ts
+++ seeds/mongo-seed.ts
@@ -9,6 +9,39 @@ const array_of = (times, generator) => {
   return Array.apply(null, Array(times)).map(() => generator());
 };
 
+const list_of_photo = [
+  'https://th-live-02.slatic.net/p/7/hequ-1483111676-123106' +
+  '5-c566b543a82cfe5a0e279dbf161bd13e-catalog_233.jpg',
+  'https://www.fjallraven.com/media/catalog/product/cache/all/base/522x/' +
+  '17f82f742ffe127f42dca9de82fb58b1/F/2/F23510-501_0.jpg',
+  'https://i.ytimg.com/vi/6d0YVd3AE7A/maxresdefault.jpg',
+  'http://www.bag-design.com/th/wp-content/uploads/2015/10/eco-bag-14.jpg',
+  'http://static.weloveshopping.com/shop/client/000054/rakbag/Sunny-683beige.jpg',
+  'https://media.shopat24.com/SWISS_GEAR_KW_044_17_/plist/812192_010_01.jpg',
+  'http://fb1-e.lnwfile.com/_/e/_raw/mc/p4/hs.jpg',
+  'http://www.cleothailand.com/wp-content/uploads/2017/03/vanmakeup.jpg',
+  'https://cache.gmo2.sistacafe.com/images/uploads/summary/image/2218/144047' +
+  '4904-il_fullxfull.579898122_8i89.jpg',
+  'http://xn--72ca1ci1cwcl0bg2l5d.ran4u.com/userfiles/fckeditor/9905/image/%E0%B' +
+  '8%A3%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%97%E0%B9%89%E0%B8%B2%E0%B9%81%E0%B8%9F%' +
+  'E0%B8%8A%E0%B8%B1%E0%B9%88%E0%B8%99%20%E0%B8%97%E0%B8%A3%E0%B8%87%20Boart%20Shoes%20C041.jpg',
+  'https://i.pinimg.com/736x/76/97/bf/7697bf24d1504cb5d928d08f0379da55--nike-air-max-air-maxes.jpg',
+  'https://th-live-02.slatic.net/p/7/ccjeans-a005-white-black-1488943952-25827721' +
+  '-0e7268f29102c8723fdc5f2bc9253c6e-product_340x340.jpg',
+  'http://www.mixytrend.com/attachments/product/images_1-1953773.jpg',
+  'http://www.mixytrend.com/attachments/product/images_1-1944858.jpg',
+  'https://f.ptcdn.info/144/041/000/o4fn49dowqyEVv46x81-o.jpg',
+  // 'https://th-live.slatic.net/p/7/kaangekngsaelkhe-wyuued-4009-9009409-ab7fca3d9e73' +
+  // 'fb0cd3674c958ec21c58-webp-zoom_850x850.jpg',
+  'https://pbs.twimg.com/media/CyrAVXoWgAAkK7f.jpg',
+  'https://th-live-01.slatic.net/p/7/orawan-closet-1453777645-9137834-1-product.jpg',
+  'https://www.daradaily.com/module/ckfinder/userfiles/images/Gucci.jpg',
+  'https://th-live-03.slatic.net/p/7/good-no01-white-red-1477598042-30347' +
+  '78-40f2862575713c4af13f9311e749ec64-product.jpg',
+  'https://th-live-02.slatic.net/p/7/wonderful-story-v2-black-1487311' +
+  '424-72902911-dc6fe5a76db1271801bfe09daefc8587-catalog_233.jpg',
+];
+
 const start_date = m.moment;
 const end_date = start_date.add(Math.floor(Math.floor(Math.random() * 60)), 'days');
 
@@ -17,7 +50,7 @@ export async function seed(database: Database) {
   await database.connection.dropDatabase();
   // Start seeding
   for (let i = 0; i <= 20; i++) {
-    const user = await database.User.create({
+    const seller = await database.User.create({
       first_name: m.first_name,
       last_name: m.last_name,
       tel_number: m.phone,
@@ -26,55 +59,25 @@ export async function seed(database: Database) {
         password: m.password,
       },
       avatar: m.url,
+      type: 'CoSeller',
     });
 
     // mm.ra
     for (let j = 0; j <= Math.floor(Math.random() * 4); j++) {
-      await database.Product.create({
+      const product = await database.Product.create({
         name: m.word,
         description: m.description,
         type: 'BuyNowProduct',
         // price: m.integer(100, 2000),
 
-        pictures: array_of(Math.floor(Math.random() * 3) + 1, () => m.random_element([
-          'https://th-live-02.slatic.net/p/7/hequ-1483111676-123106' +
-          '5-c566b543a82cfe5a0e279dbf161bd13e-catalog_233.jpg',
-          'https://www.fjallraven.com/media/catalog/product/cache/all/base/522x/' +
-          '17f82f742ffe127f42dca9de82fb58b1/F/2/F23510-501_0.jpg',
-          'https://i.ytimg.com/vi/6d0YVd3AE7A/maxresdefault.jpg',
-          'http://www.bag-design.com/th/wp-content/uploads/2015/10/eco-bag-14.jpg',
-          'http://static.weloveshopping.com/shop/client/000054/rakbag/Sunny-683beige.jpg',
-          'https://media.shopat24.com/SWISS_GEAR_KW_044_17_/plist/812192_010_01.jpg',
-          'http://fb1-e.lnwfile.com/_/e/_raw/mc/p4/hs.jpg',
-          'http://www.cleothailand.com/wp-content/uploads/2017/03/vanmakeup.jpg',
-          'https://cache.gmo2.sistacafe.com/images/uploads/summary/image/2218/144047' +
-          '4904-il_fullxfull.579898122_8i89.jpg',
-          'http://xn--72ca1ci1cwcl0bg2l5d.ran4u.com/userfiles/fckeditor/9905/image/%E0%B' +
-          '8%A3%E0%B8%AD%E0%B8%87%E0%B9%80%E0%B8%97%E0%B9%89%E0%B8%B2%E0%B9%81%E0%B8%9F%' +
-          'E0%B8%8A%E0%B8%B1%E0%B9%88%E0%B8%99%20%E0%B8%97%E0%B8%A3%E0%B8%87%20Boart%20Shoes%20C041.jpg',
-          'https://i.pinimg.com/736x/76/97/bf/7697bf24d1504cb5d928d08f0379da55--nike-air-max-air-maxes.jpg',
-          'https://th-live-02.slatic.net/p/7/ccjeans-a005-white-black-1488943952-25827721' +
-          '-0e7268f29102c8723fdc5f2bc9253c6e-product_340x340.jpg',
-          'http://www.mixytrend.com/attachments/product/images_1-1953773.jpg',
-          'http://www.mixytrend.com/attachments/product/images_1-1944858.jpg',
-          'https://f.ptcdn.info/144/041/000/o4fn49dowqyEVv46x81-o.jpg',
-          // 'https://th-live.slatic.net/p/7/kaangekngsaelkhe-wyuued-4009-9009409-ab7fca3d9e73' +
-          // 'fb0cd3674c958ec21c58-webp-zoom_850x850.jpg',
-          'https://pbs.twimg.com/media/CyrAVXoWgAAkK7f.jpg',
-          'https://th-live-01.slatic.net/p/7/orawan-closet-1453777645-9137834-1-product.jpg',
-          'https://www.daradaily.com/module/ckfinder/userfiles/images/Gucci.jpg',
-          'https://th-live-03.slatic.net/p/7/good-no01-white-red-1477598042-30347' +
-          '78-40f2862575713c4af13f9311e749ec64-product.jpg',
-          'https://th-live-02.slatic.net/p/7/wonderful-story-v2-black-1487311' +
-          '424-72902911-dc6fe5a76db1271801bfe09daefc8587-catalog_233.jpg',
-        ])),
+        pictures: array_of(Math.floor(Math.random() * 3) + 1, () => m.random_element(list_of_photo)),
         hashtags: array_of(Math.floor(Math.random() * 3), () => m.random_element([
           'uniqlo', 'HandM', 'AIIZ', 'GAP', 'Crocs', 'anello', 'kanken',
         ])),
-        colors: array_of(Math.floor(Math.random() * 3), () => {
+        colors: array_of(Math.floor(Math.random() * 3) + 1, () => {
           return { color: m.random_element(['red', 'blue', 'green']) };
         }),
-        sizes: array_of(Math.floor(Math.random() * 3), () => {
+        sizes: array_of(Math.floor(Math.random() * 3) + 1, () => {
           return { size: m.random_element(['S', 'M', 'L', 'XL', 'XXL']) };
         }),
         categories: array_of(Math.floor(Math.random() * 2) + 1, () => {
@@ -87,8 +90,29 @@ export async function seed(database: Database) {
 
         promotion_start: start_date.toDate(),
         promotion_end: end_date.toDate(),
-        owner: user._id,
+        owner: seller._id,
       });
+
+      for (let k = 0; k <= Math.floor(Math.random() * 5); k++) {
+        const user = await database.User.create({
+          first_name: m.first_name,
+          last_name: m.last_name,
+          tel_number: m.phone,
+          account: {
+            email: m.email,
+            password: m.password,
+          },
+          avatar: m.url,
+          type: 'User',
+        });
+        await database.Receipt.create({
+          buyer: user._id,
+          product: product._id,
+          size: product.sizes[0],
+          color: product.colors[0],
+          number_of_items: Math.floor(Math.random() * 5) + 1,
+        });
+      }
     }
   }
 
--- src/schema/types/OrderReceipt/resolver.ts
+++ src/schema/types/OrderReceipt/resolver.ts
@@ -2,6 +2,36 @@ import { IResolver } from '../index';
 
 const resolver: IResolver<any, any> = {
   OrderReceipt: {
+    numberOfItems({ number_of_items }) {
+      return number_of_items;
+    },
+    deliverAddress({ deliver_address }) {
+      return deliver_address;
+    },
+    trackingId({ tracking_id }) {
+      return tracking_id;
+    },
+    paymentMethod({ payment_method }) {
+      return payment_method;
+    },
+    paymentCompletedAt({ payment_completed_at }) {
+      return payment_completed_at;
+    },
+    paymentCompleted({ payment_completed }) {
+      return payment_completed;
+    },
+    productDeliveredAt({ product_delivered_at }) {
+      return product_delivered_at;
+    },
+    productDelivered({ product_delivered }) {
+      return product_delivered;
+    },
+    productReceivedAt({ product_received_at }) {
+      return product_received_at;
+    },
+    productReceived({ product_received }) {
+      return product_received;
+    },
     async product({ product }, _, { database }) {
       return await database.Product.findOne({ _id: product });
     },
