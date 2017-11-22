96b32375cab7d6b351db530817eecd8071fb3f4f
player <ammarinjtk@outlook.com>
2017-11-22 22:25:49 +0700

    Fix product and orderReceipt mutation

--- src/schema/types/Mutation/resolver.ts
+++ src/schema/types/Mutation/resolver.ts
@@ -2,39 +2,70 @@ import { IResolver } from '../index';
 
 const resolver: IResolver<any, any> = {
   Mutation: {
-    async createProduct(_, { input: { promotionStart, promotionEnd, ...input } }, { database }) {
+    async createProduct(_, { input: { promotionStart, promotionEnd, price, ...input } }, { database, user }) {
       return await database.Product.insert({
         ...input,
         promotion_start: promotionStart,
         promotion_end: promotionEnd,
+        owner: user._id
+
       });
     },
 
     async editProduct(_, { input: { product, ...input } }, { database }) {
-      await database.Product.update({ _id: product }, {
-        $set: {
-          ...input,
-        },
+      database.Product.findOne({ _id: product }).then(async (instance) => {
+        await database.Product.update({ _id: instance._id },
+          {
+            $set: {
+              ...input,
+            },
+          });
       });
       return await database.Product.findOne({ _id: product });
     },
 
-    async createOrderReceipt(_, { input: { numberOfItems, deliverAddress, paymentMethod, ...input } }, { database }) {
+    async createOrderReceipt(_, { input: { numberOfItems, deliverAddress, paymentMethod, ...input } }, { database, user }) {
+      if (database.User.find({ _id: user._id, payment_method: { _id: paymentMethod } })) {
+        input.payment_method = paymentMethod;
+      } else {
+        throw new Error(`Invalid paymentMethod ID`);
+      }
+      if (database.User.find({ _id: user._id, deliver_address: { _id: deliverAddress } })) {
+        input.deliver_address = deliverAddress;
+      } else {
+        throw new Error(`Invalid deliverAddress ID`);
+      }
       return await database.Receipt.insert({
         ...input,
         number_of_items: numberOfItems,
-        deliver_address: deliverAddress,
-        payment_method: paymentMethod,
+        buyer: user._id,
       });
     },
 
-    async editOrderReceipt(_, { input: { receipt, ...input } }, { database }) {
-      await database.Receipt.update({ _id: receipt }, {
-        $set: {
-          ...input,
-        },
+    async editOrderReceipt(_, { input: { receipt, paymentCompleted, productDelivered, productReceived, ...input } }, { database }) {
+      database.Receipt.findOne({ _id: receipt }).then(async (instance) => {
+        if ((paymentCompleted === true) && (instance.payment_completed === false)) {
+          input.paymentCompletedAt = new Date();
+        } else {
+          throw new Error(`Invalid change in paymentMethodCompleted`);
+        }
+        if ((productDelivered === true) && (instance.product_delivered === false)) {
+          input.productDeliveredAt = new Date();
+        } else {
+          throw new Error(`Invalid change in productDelivered`);
+        }
+        if ((productReceived === true) && (instance.product_received === false)) {
+          input.productReceivedAt = new Date();
+        } else {
+          throw new Error(`Invalid change in productReceived`);
+        }
+        await database.Receipt.update({ _id: instance._id }, {
+          $set: {
+            ...input,
+          },
+        });
+        return await database.Receipt.findOne({ _id: receipt });
       });
-      return await database.Receipt.findOne({ _id: receipt });
     },
   },
 };
